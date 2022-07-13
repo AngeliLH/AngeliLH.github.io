200d0d78edb248b16da5eef3274ef51f313380c5
Usuario Campo Mourao <uept11-user@aluno.pr.senac.br>
2022-07-13 10:54:30 -0300

    Adicionado o estilo de hover.

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
 function exibeNome(){
-    let nome = document.getElementById("c1 campo-nome").value
-    document.getElementById("resultadoNome").innerHTML = "Opa! bom dia, " + nome + "!"
+    let nome = document.getElementById("c1-campo-nome").value
+    document.getElementById("c1-resultadoNome").innerHTML = "Opa! bom dia, " + nome + "!"
     // alert(nome)
     // console.log(nome)
 }
 
 function parOuImpar(){
-    let number = document.getElementById("c2 espaço-numero").value
+    let number = document.getElementById("c2-espaço-numero").value
     if (number%2 == 0) {
-        document.getElementById("resultadoNumero").innerHTML = "O número " + number + " é par!"
+        document.getElementById("c2-resultadoNumero").innerHTML = "O número " + number + " é par!"
       } else {
-        document.getElementById("resultadoNumero").innerHTML = "O número " + number + " é impar!"
+        document.getElementById("c2-resultadoNumero").innerHTML = "O número " + number + " é impar!"
       }
 }
 
 function geraAleatorio(){
-    let min = document.getElementById("c3 min").value
-    let max = document.getElementById("c4 max").value
+    let min = document.getElementById("c3-min").value
+    let max = document.getElementById("c4-max").value
     let quantia = document.getElementById("c5 quantidade").value
     let valAleatorios = [];
     for (let i = 0; i < quantia; i++){
       valAleatorios.push(Math.floor(Math.random() * (max - min) + min));
     }
-    document.getElementById("resultado").innerHTML = valAleatorios
+    document.getElementById("c3-resultado").innerHTML = valAleatorios
 }
 
-function geraMega(){
-  let quantidade = document.getElementById("quantia").value
-  let nAleatorios = [];
-  for (let i = 0; i < quantidade; i++){
-    nAleatorios.push(Math.floor(Math.random() * (max - min) + min ));
+function geraJogoMega(){
+  let nJogos = parseInt(document.getElementById("c6-campo-numerico").value)
+  let JogosMega
+  for (let i = 0; i < nJogos; i++){
+    JogosMega.push("jogo" + 1)
   }
-  document.getElementById("resultadoMega").innerHTML = nAleatorios
+  document.getElementById("c4-resultadoMega").innerHTML = JogosMega
+}
+
+function CriaJogoValido(){
+  
 }
